9f6ae014dc250d4563230ed94f13712a294d5b2d
akesson <henrik@akesson.mobi>
2016-04-11 21:27:22 +0200

    Added Firebase to the dependencies and initialize it

--- app/src/main/kotlin/mobi/akesson/jdm/MainActivity.kt
+++ app/src/main/kotlin/mobi/akesson/jdm/MainActivity.kt
@@ -2,11 +2,13 @@ package mobi.akesson.jdm
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.client.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Firebase.setAndroidContext(this)
     }
 }
