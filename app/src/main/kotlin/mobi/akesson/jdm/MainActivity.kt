e9ca8050277a5b3ff562c248b94ed6135907d5f2
Eric Truong <eric.truong@ratp.fr>
2016-05-04 13:27:08 +0200

    Replace MainActivity XML layout with Anko DSL

--- app/src/main/kotlin/mobi/akesson/jdm/MainActivity.kt
+++ app/src/main/kotlin/mobi/akesson/jdm/MainActivity.kt
@@ -1,12 +1,25 @@
 package mobi.akesson.jdm
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MainActivityUI().setContentView(this)
+    }
+}
+
+class MainActivityUI : AnkoComponent<MainActivity> {
+
+    override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+        relativeLayout {
+            horizontalPadding = dimen(R.dimen.activity_horizontal_margin)
+            verticalPadding = dimen(R.dimen.activity_vertical_margin)
+
+            textView("Hello Jeux!")
+        }
     }
 }
