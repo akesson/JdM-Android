0359643892ad8926b255d56206ccf0a5f6f19b83
Eric Truong <eric.truong@ratp.fr>
2016-05-12 17:27:41 +0200

    wip

--- app/src/main/kotlin/mobi/akesson/jdm/ui/game/GameFragment.kt
+++ app/src/main/kotlin/mobi/akesson/jdm/ui/game/GameFragment.kt
@@ -3,10 +3,12 @@ package mobi.akesson.jdm.ui.game
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
+import com.firebase.client.Firebase
+import com.firebase.ui.FirebaseRecyclerAdapter
 import mobi.akesson.jdm.R
+import mobi.akesson.jdm.data.model.GameData
 import mobi.akesson.jdm.domain.model.Game
 import mobi.akesson.jdm.presenter.GamePresenter
-import mobi.akesson.jdm.ui.game.GameAdapter
 import mobi.akesson.jdm.ui.core.fragment.RecyclerFragment
 import mobi.akesson.jdm.ui.core.fragment.RecyclerFragmentUI
 import mobi.akesson.jdm.ui.core.view.GameView
@@ -15,11 +17,24 @@ import org.jetbrains.anko.textResource
 
 class GameFragment : RecyclerFragment(), GameView {
 
-    var adapter: GameAdapter? = null
+//    var adapter: FirebaseRecyclerAdapter? = null
     var presenter: GamePresenter? = null
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-        adapter = GameAdapter()
+//        adapter = GameAdapter()
+        var adapter = object : FirebaseRecyclerAdapter<GameData, GameViewHolder>(GameData::class.java, android.R.layout.simple_list_item_2, GameViewHolder::class.java, Firebase("https://jeuxdemidi.firebaseio.com").child("games")) {
+            override fun populateViewHolder(viewHolder: GameViewHolder, game: GameData, position: Int) {
+                viewHolder.text1?.text = game.name
+                viewHolder.text2?.text = "${game.minPlayers} à ${game.maxPlayers} joueurs"
+            }
+            //
+
+//            override
+//            fun populateViewHolder(ChatMessageViewHolder chatMessageViewHolder, ChatMessage chatMessage, int position) {
+//                chatMessageViewHolder.nameText.setText(chatMessage.getName());
+//                chatMessageViewHolder.messageText.setText(chatMessage.getMessage());
+//            }
+        };
 
         recyclerView?.setHasFixedSize(true)
         recyclerView?.layoutManager = LinearLayoutManager(ctx, LinearLayoutManager.VERTICAL, false)
@@ -41,7 +56,7 @@ class GameFragment : RecyclerFragment(), GameView {
     }
 
     override fun showGames(games: List<Game>) {
-        adapter?.swapData(games)
+//        adapter?.swapData(games)
         viewAnimator?.displayedChild = RecyclerFragmentUI.POSITION_LIST
     }
 
